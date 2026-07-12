c4c569a44d0c07e25ff124b1dbecb4b6b58dfdbe
Matias Vargas <matii.vargas@duocuc.cl>
2026-07-11 21:30:21 -0400

    Documentacion

---
+++ Documentación/05_Scripts_SQL/cloty_ubicacion_seed.sql
@@ -0,0 +1,3 @@
+-- Seed básico de regiones y comunas
+INSERT INTO region(nombre) VALUES ('Región Metropolitana de Santiago');
+INSERT INTO comuna(id_region,nombre) VALUES (1,'Santiago'),(1,'Colina'),(1,'Las Condes'),(1,'Providencia');
